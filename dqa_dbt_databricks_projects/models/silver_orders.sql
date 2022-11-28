b2d4927ab21a7233ecbada3b29f69cb67ed58ab7
Tanish Khandelwal <76485783+tknishh@users.noreply.github.com>
2022-11-28 23:28:17 +0530

    Create silver_orders.sql

---
+++ dqa_dbt_databricks_projects/models/silver_orders.sql
@@ -0,0 +1,11 @@
+with silver_orders as (
+    select 
+       order_id, 
+       date(order_date_time) as order_date,
+       year(order_date_time) as order_year, 
+       month(order_date_time) as order_month,
+       day(order_date_time) as order_day_of_month 
+    from {{ref('bronze_orders')}}
+)
+select * 
+from silver_orders
